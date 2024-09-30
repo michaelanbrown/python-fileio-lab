ebe364e6f5da1c10ab69f6b47f6f2685a8f92ed7
michaelanbrown <m_brownbrown@yahoo.com>
2024-09-30 16:07:37 -0400

    appending a file

--- lib/file_io.py
+++ lib/file_io.py
@@ -4,6 +4,7 @@ def write_file(file_name, file_content):
 
 def append_file(file_name, append_content):
     with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
     pass
